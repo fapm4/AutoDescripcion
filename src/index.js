be479da3ce75d87da8468d75c3006981e63707f1
fapm4 <fapm4@alu.ua.es>
2023-02-01 13:26:51 +0100

    Empezando

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 'use strict'
 
-// Importamos Electron
-const { app, BrowserWindow } = require('electron');
+/////////////////////////// Imports ///////////////////////////
+// Electron
+const { app, BrowserWindow, Menu} = require('electron');
+
+// URL
 const url = require('url');
+
+// Path
 const path = require('path');
 
+// Electron Reload
+require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+});
+/////////////////////////// Código ///////////////////////////
+
+// Ventana principal con alcance global
 let ventanaPrincipal;
 
-// Creamos y cargamos la página principal
+// Cuando la aplicación esté lista
 app.on('ready', () => {
+    // Se crea la ventana principal
     ventanaPrincipal = new BrowserWindow({});
+    
+    // Se carga el archivo index.html
     ventanaPrincipal.loadURL(url.format({
-        pathname: path.joint(__dirname, 'views', 'index.html'),
-        protocol: file,
+        pathname: path.join(__dirname, 'views', 'index.html'),
+        protocol: 'file',
         slashes: true
     }));
+
+    // Se crea el menú de la aplicación
+    Menu.setApplicationMenu(Menu.buildFromTemplate([]));
 });
